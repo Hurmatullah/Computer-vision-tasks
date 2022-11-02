06799a55aff3b5adf24ded8d2d4655019f6c40a8
= <hrasaa1@gmail.com>
2022-11-01 23:52:24 -0700

    Task 11

---
+++ task10.py
@@ -0,0 +1,13 @@
+#сместить изображение но 10 пикселей вправо
+import cv2
+import numpy as np
+
+image = cv2.imread('images.jpg')
+shift = 10
+
+for i in range(image.shape[1] -1, image.shape[1] - shift, -1):
+    image = np.roll(image, 1, axis= 1)
+    image[:, -1] = 0
+
+cv2.imshow('image', image)
+cv2.waitKey()
