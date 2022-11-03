2f16fabed70a25272a455b4479e7be8c5221c349
= <hrasaa1@gmail.com>
2022-11-03 01:11:50 -0700

    Update task4

--- task4.py
+++ task4.py
@@ -1,9 +1,11 @@
 #перевести в grayscale
 import cv2 as cv
-import numpy as np
 
+#Поиск изображения по источнику
 image = cv.imread('images.jpg')
+#Преобразуйте цвет исходного изображения в серый цвет
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+#Показ нашего изображения на экране
 cv.imshow('Gray Image', gray)
 cv.waitKey(0)
 cv.destroyAllWindows()
