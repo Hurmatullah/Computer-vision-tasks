3debbdae7b0a2080f6cc42a3276210f94dc0ee86
= <hrasaa1@gmail.com>
2022-11-03 01:04:09 -0700

    Update Task2

--- task2.py
+++ task2.py
@@ -1,9 +1,13 @@
 #найти все sift features точки на изображении
 import cv2 as cv
 
+# Поиск изображения по источнику
 image = cv.imread('images.jpg')
-gray= cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+# Преобразование цвета из BGR в серый
+cvColor= cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+# способ инициирования просеивания
 sift = cv.SIFT_create()
-kp = sift.detect(gray,None)
-image=cv.drawKeypoints(gray,kp,image)
+# Обнаружение преобразованного цвета
+siftDetect = sift.detect(cvColor,None)
+image=cv.drawKeypoints(cvColor,siftDetect,image)
 cv.imwrite('task2-image/sift_keypoints.jpg',image)
