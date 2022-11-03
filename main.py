18e08f09f04e4238c3eb8a51efa1ad44a63c7d02
= <hrasaa1@gmail.com>
2022-11-03 00:59:41 -0700

    Task1 code cleaned

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 #найти все orb features точки на изображении
 import cv2 as cv
-import numpy as np
 from matplotlib import pyplot as plt
 
+# Считывающее изображение
 image = cv.imread("images.jpg", 0)
-
-#initiating orb
+# Инициирующий ШАР
 orb = cv.ORB_create()
-# find the keypoints with ORB
+# Обнаружение ключевых точек с помощью шара
 kp = orb.detect(image,None)
-# compute the descriptors with ORB
+# вычислите дескрипторы с помощью ORB
 kp, des = orb.compute(image, kp)
-# draw only keypoints location,not size and orientation
+# Рисование ключевых моментов
 img2 = cv.drawKeypoints(image, kp, None, color=(0,255,0), flags=0)
 plt.imshow(img2), plt.show()
