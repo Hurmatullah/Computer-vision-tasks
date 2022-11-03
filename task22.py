b8b967134c3c3918792960c3465e56b134865d9d
= <hrasaa1@gmail.com>
2022-11-03 03:24:05 -0700

    Last update

--- task22.py
+++ task22.py
@@ -1,19 +1,21 @@
 # сделать фильтрацию изображения при помощи Фурье преобразоваия, оставить только быстрые частоты
 import numpy as np
-import cv2
+import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images.jpg',0)
+img = cv.imread('images.jpg',0)
 
 img_float32 = np.float32(img)
 
-dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
+dft = cv.dft(img_float32, flags = cv.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 
-magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.title('High frequence'), plt.xticks([]), plt.yticks([])
 plt.show()
+cv.imwrite('task22-image/Original_image.jpg', img)
+cv.imwrite('task22-image/HighFrequency.jpg', magnitude_spectrum)
--- task23.py
+++ task23.py
@@ -1,13 +1,13 @@
 # - сделать фильтрацию изображения при помощи Фурье преобразоваия,оставить только медленные частоты
 import numpy as np
-import cv2
+import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images.jpg',0)
+img = cv.imread('images.jpg',0)
 
 img_float32 = np.float32(img)
 
-dft = cv2.dft(img_float32, flags = cv2.DFT_COMPLEX_OUTPUT)
+dft = cv.dft(img_float32, flags = cv.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 
 rows, cols = img.shape
@@ -20,12 +20,11 @@ mask[crow-30:crow+30, ccol-30:ccol+30] = 1
 # применить маску и обратное ДПФ
 fshift = dft_shift*mask
 f_ishift = np.fft.ifftshift(fshift)
-img_back = cv2.idft(f_ishift)
-img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+img_back = cv.idft(f_ishift)
+img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
 
 plt.subplot(121),plt.imshow(img, cmap = 'gray')
 plt.title('Original image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
 plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-
 plt.show()
