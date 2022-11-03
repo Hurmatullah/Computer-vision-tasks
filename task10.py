565fc6a730e306982cdfd6817527c8c2fd068f43
= <hrasaa1@gmail.com>
2022-11-03 01:32:06 -0700

    Update task10

--- task10.py
+++ task10.py
@@ -2,9 +2,11 @@
 import cv2
 import numpy as np
 
+#Поиск изображения по источнику
 image = cv2.imread('images.jpg')
 shift = 10
 
+#Смещаем наше изображение на 10 пикселей вправо
 for i in range(image.shape[1] -1, image.shape[1] - shift, -1):
     image = np.roll(image, 1, axis= 1)
     image[:, -1] = 0
