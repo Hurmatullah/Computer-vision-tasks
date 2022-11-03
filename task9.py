93d778b0fc3dd4d290a844d787a9f790a77b970a
= <hrasaa1@gmail.com>
2022-11-03 01:30:38 -0700

    Update task9

--- task9.py
+++ task9.py
@@ -1,14 +1,13 @@
 # повернуть изображение на 30 градусов
 import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
 
+#Поиск изображения по источнику
 image = cv.imread('images.jpg')
 (h, w) = image.shape[:2]
 center = (w / 2, h / 2)
 angle = 30
 scale = 1
-
+#Поворот изображения на 30 градусов
 M = cv.getRotationMatrix2D(center, angle, scale)
 rotated = cv.warpAffine(image, M, (w, h))
 cv.imwrite('task9-image/30_degree_image.jpg', rotated)
